2888c7dbb7a39897a2934baa70bdaf8e5433b7ea
DominikMendoza <u20201b980@upc.edu.pe>
2023-11-13 16:39:34 -0500

    process

--- app/domain/domain.py
+++ app/domain/domain.py
@@ -1,12 +1,3 @@
 class ProcesadorDatos:
     def procesar(self, datos):
-        """
-        /*
-        datos = {
-            "hola" : "hola",
-            "nombre" : "nombre"
-        }
-        """
-        # Aquí puedes realizar cualquier procesamiento adicional necesario
-        # Devuelves los datos procesados
         return datos
